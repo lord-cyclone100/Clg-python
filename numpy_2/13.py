f66a9b33acade8b741815be5e8130891de0c520e
lord-cyclone100 <sang66deb@gmail.com>
2024-01-07 21:30:02 +0530

    Added new code

---
+++ numpy_2/13.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+# Mean
+mean_value = np.mean(arr)
+print("Mean:", mean_value)
+
+# Median
+median_value = np.median(arr)
+print("Median:", median_value)
+
+# Standard Deviation
+std_deviation_value = np.std(arr)
+print("Standard Deviation:", std_deviation_value)
