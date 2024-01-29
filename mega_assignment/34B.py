bce9129e81311e8597efabcbd1d63a3ce27eaef4
lord-cyclone100 <sang66deb@gmail.com>
2024-01-29 13:28:34 +0530

    Added new code

---
+++ mega_assignment/34B.py
@@ -0,0 +1,14 @@
+#Generate 30 random 2-d points and draw it using a scatter plot.
+
+import matplotlib.pyplot as plt
+import random
+
+x_list = []
+y_list = []
+
+for i in range(30):
+    x_list.append(random.randint(1,100))
+    y_list.append(random.randint(1,100))
+
+plt.plot(x_list,y_list)
+plt.show()
