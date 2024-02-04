ceb2019d922a20cb72ac5bcb4fca4373a0a41d08
lord-cyclone100 <sang66deb@gmail.com>
2024-02-04 11:18:53 +0530

    Added new code

--- mega_assignment/11A.py
+++ mega_assignment/11A.py
@@ -8,10 +8,22 @@ def check_palindrome(n):
         rev = (rev*10)+r
         n = n // 10
     if(rev == num):
-        print(num,end=" ")
+        return 1
+    else:
+        return 0
+
+def check_prime(n):
+    count = 0
+    for i in range(2,n):
+        if(n%i==0):
+            count += 1
+    if(count==0):
+        p = check_palindrome(n)
+        if(p==1):
+            print(f"{n} is a palindromic prime number")
 
 a = int(input("Enter lower bound:"))
 b = int(input("Enter upper bound:"))
 
 for i in range(a,b+1):
-    check_palindrome(i)
+    check_prime(i)
