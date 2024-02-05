f01ce02be9c6c76193c38d9d853562120b2adb1c
lord-cyclone100 <sang66deb@gmail.com>
2024-02-05 20:40:09 +0530

    Added new code

--- mega_assignment/31B.py
+++ mega_assignment/31B.py
@@ -1,3 +1,6 @@
+'''Create the following lists using a for loop:
+The list ['a','bb','ccc','dddd',...], that ends with 26 copies of the letter z.'''
+
 def letter_printer():
     letter_list = []
     for i in range(26):
