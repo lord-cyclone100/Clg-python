dc3d36eccfc2d33e0ff9ad6ed23ca79c5874bb5c
lord-cyclone100 <sang66deb@gmail.com>
2024-02-05 23:21:48 +0530

    Added new code

--- mega_assignment/32B.py
+++ mega_assignment/32B.py
@@ -1,33 +1,15 @@
 '''Program to check a number is ugly or not. Ugly numbers are positive numbers whose only
 prime factors are 2, 3 or 5.'''
 
-n=int(input("Enter a number : "))
-
-def isPrime(x): 
-    f=0
-    for i in range(2,x//2):
-        if x%i==0:
-            f+=1
-    if f==0:
+def uglyNum(n):
+    if n==1:
         return True
-    else:
-        return False
-    
-def checkugly():
-    f=0
-    for i in range(1,n+1):
-        if(n%i==0 and isPrime(i)):
-            if(i==2 or i==3 or i==5):
-                f=0
-            else: 
-                f+=1
-    return f
+    if not n%2 or not n%3 or not n%5:
+        return True
+    return False
 
-def main():
-    x=checkugly()   
-    if x==0 or n==1:
-        print(n,"is ugly") 
-    else:
-        print(n,"is not ugly")  
-
-main()                               
+a = int(input("Enter a number: "))
+if(uglyNum(a)):
+    print("The Number is Ugly")
+else:
+    print("The Number is not Ugly")
