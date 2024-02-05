b43870421a1df9d9dd6d1b826b5a7a9644e6f732
lord-cyclone100 <sang66deb@gmail.com>
2024-02-05 23:22:19 +0530

    Added new code

--- mega_assignment/28B.py
+++ mega_assignment/28B.py
@@ -2,5 +2,12 @@
 print the electricity consumption for July and November month.'''
 
 with open("28B.txt","r") as file:
-    data = file.readlines()
-    print(f"{data[6]}\n{data[10]}")
+    title = file.readline()
+    data1 = file.readline().split(",")
+    data2 = file.readline().split(",")
+    data3 = file.readline().split(",")
+    print(data1,data2,data3)
+
+print("Customer1-> July:",data1[7],"November:",data1[11])
+print("Customer2-> July:",data2[7],"November:",data2[11])
+print("Customer3-> July:",data3[7],"November:",data3[11])
